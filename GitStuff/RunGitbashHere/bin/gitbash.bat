b7a2c6241e64d0fd012a373cfd775a9b89fd2989
AstroFP <109758910+AstroFP@users.noreply.github.com>
2025-06-19 22:37:00 +0200

    add: bat file for running gitbash in current directory

---
+++ GitStuff/RunGitbashHere/bin/gitbash.bat
@@ -0,0 +1,19 @@
+@echo off
+setlocal enabledelayedexpansion
+
+set "originalPath=%CD%"
+
+for /f "delims=" %%G in ('where git.exe') do (
+
+   for %%A in ("%%G") do (
+      set gitPath=%%~dpA
+   )  
+)
+
+REM redirection from Git\cmd to Git\
+set gitPath=!gitPath!
+cd /d "%gitPath%"
+cd ..
+set gitPath=%cd%
+
+start "" "%gitPath%\git-bash.exe" --cd=%originalPath%
